9c9a8d362cadfae67c0c991f82524289c07426b1
Benjamin Pinard <benpin31@gmail.com>
2021-04-12 13:03:10 +0200

    move div with mouth

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import DragAndDrop1 from './components/DragAndDrop1'
+
+function App() {
+  return (
+    <div>
+      <DragAndDrop1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DragAndDrop1.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+
+import "./../styles/DragAndDrop1.css";
+
+export default class DragAndDrop1 extends Component {
+  state = {
+    targeted: null,
+    initialMousePosition: null,
+    initialPositions: [{ id: "s1" }, { id: "s2" }],
+    styles: ["s1", "s2"].map((id) => {
+      return { id: id, position: "static", left: 0, top: 0 };
+    }),
+  };
+
+  handleMouseMove = (event) => {
+    event.preventDefault();
+
+    if (event.buttons === 1) {
+      const {
+        targeted,
+        styles,
+        initialPositions,
+        initialMousePosition,
+      } = this.state;
+
+      const style = styles.find((style) => style.id === targeted);
+      const initialPosition = initialPositions.find(
+        (pos) => pos.id === targeted
+      );
+
+      style.position = "fixed";
+      style.left =
+        initialPosition.initialLeft + event.clientX - initialMousePosition[0];
+      style.top =
+        initialPosition.initialTop + event.clientY - initialMousePosition[1];
+
+      this.setState({ styles });
+    }
+  };
+
+  handleSetInitialPosition = (event) => {
+    const { initialPositions } = this.state;
+
+    const pos = initialPositions.find((pos) => pos.id === event.target.id);
+    pos.initialLeft = event.target.getBoundingClientRect().left;
+    pos.initialTop = event.target.getBoundingClientRect().top;
+
+    this.setState({
+      targeted: event.target.id,
+      initialPositions: initialPositions,
+      initialMousePosition: [event.clientX, event.clientY],
+    });
+  };
+
+  getStyle(style) {
+    return {
+      position: style.position,
+      left: style.left,
+      top: style.top,
+    };
+  }
+
+  render() {
+    const { styles } = this.state;
+
+    return (
+      <div
+        className="DragAndDrop1__container"
+      >
+        <div
+          className="square"
+          id="s1"
+          onMouseDown={this.handleSetInitialPosition}
+          onMouseMove={this.handleMouseMove}
+          style={this.getStyle(styles.find((style) => style.id === "s1"))}
+        ></div>
+        <div
+          className="square"
+          id="s2"
+          onMouseDown={this.handleSetInitialPosition}
+          onMouseMove={this.handleMouseMove}
+          style={this.getStyle(styles.find((style) => style.id === "s2"))}
+        ></div>
+      </div>
+    );
+  }
+}
